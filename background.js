4cc23fddcd1a993e55f2c77edc1a33c15e0d68a7
dubway <dubway@gmail.com>
2014-03-25 22:26:04 -0400

    new

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+// Public Variables
+var selectedCategories;
+
+// XML Parser
+var xmlDoc=loadXMLDoc("http://onemorepixel.cc/content/game.xml");
+var main = xmlDoc.getElementsByTagName("library");
+var quote = xmlDoc.getElementsByTagName("quote");
+var author = xmlDoc.getElementsByTagName("author");
+
+var roulette;
+var icon;
+
 function loadXMLDoc(dname) 
 {
 if (window.XMLHttpRequest)
@@ -14,28 +26,123 @@ return xhttp.responseXML;
 }
 
 function show() {
-  //load XML
-  var xmlDoc=loadXMLDoc("http://onemorepixel.cc/content/game.xml");
-  var main = xmlDoc.getElementsByTagName("library");
-  var quote = xmlDoc.getElementsByTagName("quote")[1];
-  var author = xmlDoc.getElementsByTagName("author")[1].childNodes[0].nodeValue;
 
-  var randomNum = Math.floor(Math.random() * (main.length - 0 + 1)) + 0;
-  var text = xmlDoc.getElementsByTagName("text")[randomNum].childNodes[0].nodeValue;
+  checkRoulette();
+  checkCategory();
+
+  var randomNum = randRange(0,(selectedCategories.length-1));
+  var cat =  selectedCategories[randomNum];
+  var catName = returnCategoryName(cat)
+  var text = getCategory(catName, roulette);
+
+  //var icon = checkIcons(catName);
 
 
+  // Notification API
   var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
   var hour = time[1] % 12 || 12;               // The prettyprinted hour.
   var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
   var notification = window.webkitNotifications.createNotification(
-    '64.png',                      // The image.
-    hour + time[2] + ' ' + period, // The title.
-    text      // The body.
+    '64.png',                       // The image.
+    hour + time[2] + ' ' + period + ' | ' + catName,  // The title.
+    text                            // The body.
   );
+
+  // SHOW the Notification
   notification.show();
+  // Set Timeout to CLOSE the Notification
   setTimeout(function(){
     notification.cancel();
   }, 8000);
+
+}
+
+function checkCategory(){
+  var checkCategories = [localStorage.itp,localStorage.life,localStorage.people,localStorage.sex,localStorage.tech,localStorage.things,localStorage.work];
+  var counter = 0;
+  var sortCategories = [];
+  for (var i = 0; i < checkCategories.length; i++) {
+    if (checkCategories[i] == "true") {
+      sortCategories[counter] = i;
+      counter++;
+    };
+  };
+  selectedCategories = sortCategories;
+}
+
+function checkRoulette(){
+  roulette = localStorage.roulette;
+  //console.log(roulette);
+}
+
+function checkIcons(catName, byName){
+  console.log(catName + " " + byName);
+}
+
+function returnCategoryName(cat){
+  switch (cat) {
+  case 0:
+    x="itp";
+    break;
+  case 1:
+    x="life";
+    break;
+  case 2:
+    x="people";
+    break;
+  case 3:
+    x="sex";
+    break;
+  case 4:
+    x="tech";
+    break;
+  case 5:
+    x="things";
+    break;
+  case 6:
+    x="work";
+    break;
+  }
+  return x;
+}
+
+function getCategory(cat, roul){
+  var counter = 0;
+  var quotes = [];
+
+  for(var i=0; i<quote.length; i++){
+    if(roul == "true"){
+      if(quote[i].getAttribute('category') == cat){
+        txt = xmlDoc.getElementsByTagName("text")[i];
+        quotes[counter] = i;
+        counter++
+        console.log("With all the craziness!");
+
+      }
+
+    } else {
+      if(quote[i].getAttribute('category') == cat && quote[i].getAttribute('roulette') == "0"){
+        txt = xmlDoc.getElementsByTagName("text")[i];
+        quotes[counter] = i;
+        counter++
+        console.log("Without roulette");
+      }
+    }
+  }
+
+  var randomNum = randRange(0, (quotes.length-1));
+  id = quotes[randomNum];
+  txt = xmlDoc.getElementsByTagName("text")[id];
+  aut = xmlDoc.getElementsByTagName("author")[id];
+  by = aut.firstChild.nodeValue;
+  checkIcons(cat, by);
+
+  return txt.firstChild.nodeValue;
+}
+
+function randRange(min, max){
+  var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomNum;
 }
 
 // Conditionally initialize the options.
@@ -43,6 +150,17 @@ if (!localStorage.isInitialized) {
   localStorage.isActivated = true;   // The display activation.
   localStorage.frequency = 1;        // The display frequency, in minutes.
   localStorage.isInitialized = true; // The option initialization.
+
+  // Categories
+  localStorage.itp = true;
+  localStorage.life = true;
+  localStorage.people = true;
+  localStorage.sex = true;
+  localStorage.tech = true;
+  localStorage.things = true;
+  localStorage.work = true;
+
+  localStorage.roulette = true;
 }
 
 // Test for notification support.
--- options.js
+++ options.js
@@ -11,6 +11,18 @@ window.addEventListener('load', function() {
   options.frequency.value = localStorage.frequency;
                                          // The display frequency, in minutes.
 
+  // Initialize Categories
+  categories.itp.checked = JSON.parse(localStorage.itp);
+  categories.life.checked = JSON.parse(localStorage.life);
+  categories.people.checked = JSON.parse(localStorage.people);
+  categories.sex.checked = JSON.parse(localStorage.sex);
+  categories.tech.checked = JSON.parse(localStorage.tech);
+  categories.things.checked = JSON.parse(localStorage.things);
+  categories.work.checked = JSON.parse(localStorage.work);
+
+  crowdsource.roulette.checked = JSON.parse(localStorage.roulette);
+
+
   if (!options.isActivated.checked) { ghost(true); }
 
   // Set the display activation and frequency.
@@ -22,4 +34,38 @@ window.addEventListener('load', function() {
   options.frequency.onchange = function() {
     localStorage.frequency = options.frequency.value;
   };
+
+
+  categories.itp.onchange = function() {
+    localStorage.itp = categories.itp.checked;
+  };
+  categories.life.onchange = function() {
+    localStorage.life = categories.life.checked;
+  };
+  categories.people.onchange = function() {
+    localStorage.people = categories.people.checked;
+  };
+  categories.sex.onchange = function() {
+    localStorage.sex = categories.sex.checked;
+  };
+  categories.tech.onchange = function() {
+    localStorage.tech = categories.tech.checked;
+  };
+  categories.things.onchange = function() {
+    localStorage.things = categories.things.checked;
+  };
+    categories.work.onchange = function() {
+    localStorage.work = categories.work.checked;
+  };
+
+  crowdsource.roulette.onchange = function() {
+    localStorage.roulette = crowdsource.roulette.checked;
+  };
+
+
 });
+
+
+
+
+
